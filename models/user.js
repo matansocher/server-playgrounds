f11c89fae5eebfb4fb800c37244c0c52db560eee
Matan Socher <msocher@qti.qualcomm.com>
2018-03-11 16:35:40 +0200

    added mongoose files and general api

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+var UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  }
+});
+
+module.exports = {User}
